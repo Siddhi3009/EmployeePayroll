98a92ece7bf0fcbeca54c969e661960e312f8070
Administrator <siddhiseth3009@gmail.com>
2020-11-05 12:27:33 +0530

    [Siddhi]Add. AddEmployees method added

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -159,3 +159,25 @@ from Payroll payroll inner join employee emp
 on payroll.Id = emp.Id
 where emp.Gender = 'M'
 group by gender;
+--Add procedure to store data
+create procedure SpAddEmployeeDetails
+(
+@EmployeeName varchar(255),
+@PhoneNumber varchar(255),
+@Address varchar(255),
+@Department varchar(255),
+@Gender char(1),
+@BasicPay float,
+@Deductions float,
+@TaxablePay float,
+@Tax float,
+@NetPay float,
+@StartDate Date
+)
+as
+begin
+insert into employee_payroll values
+(
+@EmployeeName,@BasicPay,@StartDate,@Gender,@PhoneNumber,@Address,@Department,@Deductions,@TaxablePay,@Tax,@NetPay
+)
+end
--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -8,7 +8,19 @@ namespace EmployeePayrollService
         {
             Console.WriteLine("Welcome To Employee Payroll Service System");
             EmployeeRepo repo = new EmployeeRepo();
-            repo.GetAllEmployee();
+            EmployeeModel employee = new EmployeeModel();
+            employee.EmployeeName = "Indal";
+            employee.Department = "Tech";
+            employee.PhoneNumber = "6302907918";
+            employee.Address = "02-Khajauli";
+            employee.Gender = 'M';
+            employee.BasicPay = 10000.00M;
+            employee.Deductions = 1500;
+            employee.StartDate = employee.StartDate = Convert.ToDateTime("2020-11-03");
+
+            if (repo.AddEmployee(employee))
+                Console.WriteLine("Records added successfully");
+            //repo.GetAllEmployee();
         }
     }
 }
