2b48270299fc637d7726aaa1488f642f26e11858
Administrator <siddhiseth3009@gmail.com>
2020-11-16 13:23:43 +0530

    [Siddhi]Add. TestMethod to check employeeList retrieved added

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -9,5 +9,33 @@ namespace RestSharpTest
     [TestClass]
     public class UnitTest1
     {
+        RestClient client = new RestClient("http://localhost:3000");
+        /// <summary>
+        /// Interface to get list of employees in the json server
+        /// </summary>
+        /// <returns></returns>
+        private IRestResponse GetEmployeeList()
+        {
+            RestRequest request = new RestRequest("/employees", Method.GET);
+            //act
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+        /// <summary>
+        /// Test method to check the employee list retrieved from json server
+        /// </summary>
+        [TestMethod]
+        public void OnCallingGetApi_ReturnEmployeeList()
+        {
+            IRestResponse response = GetEmployeeList();
+            //assert
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.OK);
+            List<Employee> dataResponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
+            Assert.AreEqual(11, dataResponse.Count);
+            foreach (var item in dataResponse)
+            {
+                System.Console.WriteLine("id: " + item.id + "Name: " + item.name + "Salary: " + item.Salary);
+            }
+        }
     }
 }
