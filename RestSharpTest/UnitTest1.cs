3f00c354adadea0a0020f4ab1d95189e3bbb18cf
Administrator <siddhiseth3009@gmail.com>
2020-11-16 15:29:06 +0530

    [Siddhi]Add. TestMethod to check employee added, added

--- RestSharpTest/UnitTest1.cs
+++ RestSharpTest/UnitTest1.cs
@@ -3,6 +3,8 @@ using EmployeePayrollService;
 using RestSharp;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 
 namespace RestSharpTest
 {
@@ -36,6 +38,23 @@ namespace RestSharpTest
             {
                 System.Console.WriteLine("id: " + item.id + "Name: " + item.name + "Salary: " + item.Salary);
             }
+        }
+        [TestMethod]
+        public void GivenEmployee_WhenPosted_ShouldReturnAddedEmployee()
+        {
+            //arrange
+            RestRequest request = new RestRequest("/Employees", Method.POST);
+            JObject jObject = new JObject();
+            jObject.Add("name", "Mark");
+            jObject.Add("Salary", "20000");
+            request.AddParameter("application/json", jObject, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            //assert
+            Assert.AreEqual(response.StatusCode, System.Net.HttpStatusCode.Created);
+            Employee dataResponse = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Mark", dataResponse.name);
+            Assert.AreEqual(20000, dataResponse.Salary);
+            Console.WriteLine(response.Content);
         }
     }
 }
